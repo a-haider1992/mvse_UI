20f968a9dadecfd3628974e9bad043d8159acf7d
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-07-28 10:42:49 +0100

    File type detection.

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,29 +1,68 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent {
-  selectedImageSrcList: Array<string | ArrayBuffer | null> = [];
+export class UploadComponent implements OnDestroy {
+  selectedFiles: File[] = [];
+  selectedImages: File[] = [];
+  selectedVideos: File[] = [];
+  selectedAudios: File[] = [];
+  objectURLs: string[] = []; // Array to store the object URLs
+
+  constructor() { }
 
   @ViewChild('fileInput') fileInput!: ElementRef;
 
-  onImagesSelected(event: any) {
-    const files: FileList = event.target.files;
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.selectedImageSrcList.push(e.target.result);
-      };
-      reader.readAsDataURL(file);
+  // onImagesSelected(event: any) {
+  //   const files: FileList = event.target.files;
+  //   for (let i = 0; i < files.length; i++) {
+  //     const file = files[i];
+  //     const reader = new FileReader();
+  //     reader.onload = (e: any) => {
+  //       this.selectedImageSrcList.push(e.target.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // }
+
+  onFileSelected(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      this.selectedFiles = Array.from(inputElement.files);
+
+      // Filter files based on their extensions
+      this.selectedImages = this.selectedFiles.filter(file => this.isImageFile(file.name));
+      this.selectedVideos = this.selectedFiles.filter(file => this.isVideoFile(file.name));
+
+      this.objectURLs = this.selectedFiles.map(file => URL.createObjectURL(file));
     }
   }
 
+  isImageFile(fileName: string): boolean {
+    return fileName.toLowerCase().endsWith('.jpg')
+      || fileName.toLowerCase().endsWith('.jpeg')
+      || fileName.toLowerCase().endsWith('.png')
+      || fileName.toLowerCase().endsWith('.gif')
+      || fileName.toLowerCase().endsWith('.bmp');
+  }
+
+  isVideoFile(fileName: string): boolean {
+    return fileName.toLowerCase().endsWith('.mp4')
+      || fileName.toLowerCase().endsWith('.avi')
+      || fileName.toLowerCase().endsWith('.mov')
+      || fileName.toLowerCase().endsWith('.wmv')
+      || fileName.toLowerCase().endsWith('.mkv');
+  }
+  
+  ngOnDestroy(): void {
+    // Revoke all object URLs in the objectURLs array
+    this.objectURLs.forEach(url => URL.revokeObjectURL(url));
+  }
   clearImages() {
-    this.selectedImageSrcList = [];
+    this.selectedImages = this.selectedVideos = [];
     // Reset the file input to clear the selected files
     this.fileInput.nativeElement.value = '';
   }
