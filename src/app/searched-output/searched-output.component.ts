770f9757040f88368d38316037a14b44a9bc48fe
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-08-04 11:03:50 +0100

    Updated second page.

--- src/app/searched-output/searched-output.component.ts
+++ src/app/searched-output/searched-output.component.ts
@@ -9,9 +9,10 @@ import { DataSharingServiceService } from '../data-sharing-service.service';
 export class SearchedOutputComponent {
 
   constructor(private dataSharingService: DataSharingServiceService){}
+  selectedImages: any[] = [];
 
-  frames: any[] = ["../assets/images/image.png", "../assets/images/image.png", "../assets/images/image.png", "../assets/images/image.png", "../assets/images/image.png", "../assets/images/image.png", "../assets/images/image.png"];
-  audios: any[] = ["../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav"];
+  frames: any[] = ["../assets/images/image.png", "../assets/images/logo.png", "../assets/images/image.png", "../assets/images/logo.png", "../assets/images/logo.png"];
+  audios: any[] = ["../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav",  "../assets/audios/0.wav", "../assets/audios/0.wav"];
   videoList: string[] = [
     '../assets/videos/test2.mp4', '../assets/videos/test3.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test6.mp4', '../assets/videos/test6.mp4', '../assets/videos/test6.mp4', '../assets/videos/test6.mp4'
   ];
@@ -20,8 +21,21 @@ export class SearchedOutputComponent {
 
   ngOnInit(): void {
     this.data = this.dataSharingService.sharedData;
-    console.log("Inside second component!!");
+    // console.log("Inside second component!!");
     console.log(this.data);
+  }
+
+  toggleImageSelection(src: string): void {
+    const index = this.selectedImages.indexOf(src);
+    if (index === -1) {
+      this.selectedImages.push(src);
+    } else {
+      this.selectedImages.splice(index, 1);
+    }
+  }
+
+  isImageSelected(src: string): boolean {
+    return this.selectedImages.includes(src);
   }
   
   search(): void{
