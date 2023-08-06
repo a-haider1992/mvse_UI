4b19df2cb808372b67d3ca9ef5080b5bcf3c5e7c
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-08-06 11:50:08 +0100

    Video overlay modal added.

--- src/app/searched-output/searched-output.component.ts
+++ src/app/searched-output/searched-output.component.ts
@@ -8,21 +8,64 @@ import { DataSharingServiceService } from '../data-sharing-service.service';
 })
 export class SearchedOutputComponent {
 
-  constructor(private dataSharingService: DataSharingServiceService){}
+  constructor(private dataSharingService: DataSharingServiceService) { }
   selectedImages: any[] = [];
 
   frames: any[] = ["../assets/images/image.png", "../assets/images/logo.png", "../assets/images/image.png", "../assets/images/logo.png", "../assets/images/logo.png"];
-  audios: any[] = ["../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav",  "../assets/audios/0.wav", "../assets/audios/0.wav"];
+  audios: any[] = ["../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav", "../assets/audios/0.wav"];
   videoList: string[] = [
     '../assets/videos/test2.mp4', '../assets/videos/test3.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test2.mp4', '../assets/videos/test6.mp4', '../assets/videos/test6.mp4', '../assets/videos/test6.mp4', '../assets/videos/test6.mp4'
   ];
+  // To store posters returned by API call
+  posterList: string[] = [];
 
   data: any[] = []
+  source_video: string = " ";
+  showVideoOverlay = false;
 
   ngOnInit(): void {
     this.data = this.dataSharingService.sharedData;
-    // console.log("Inside second component!!");
     console.log(this.data);
+    this.videoList = this.data;
+  }
+
+  onPosterClick(image: any): void {
+    // Extract video source from frame "image"
+    let str2 = image;
+    let tmpArray = str2.split("/");
+    let folder = str2.substring(0, str2.lastIndexOf("/"));
+    let video_name = tmpArray[tmpArray.length - 1]; //archivesearch_5e9592bca0fe6201df2985c9_340_415.mp4
+    video_name = video_name.substring(0, video_name.lastIndexOf('_')); //archivesearch_5e9592bca0fe6201df2985c9_340  or archivesearch_5e9592bca0fe6201df2985c9-340
+    let starttimestr = "0";
+    if (video_name.indexOf("-") > 0) {
+      // archivesearch_5e9592bca0fe6201df2985c9-340
+      this.source_video = video_name.substring(0, video_name.lastIndexOf("-")) + ".mp4";
+      starttimestr = video_name.split("-")[1];
+    } else {
+      // archivesearch_5e9592bca0fe6201df2985c9_340
+      this.source_video = video_name.substring(0, video_name.lastIndexOf("_")) + ".mp4"; // archivesearch_5e9592bca0fe6201df2985c9.mp4
+      starttimestr = video_name.substring(video_name.lastIndexOf("_") + 1);
+    }
+
+    let starttime = 0;
+    if (starttimestr.indexOf(".") > 0) {
+      starttime = parseFloat(starttimestr);
+    } else {
+      starttime = parseFloat(starttimestr) / 25.0;
+      starttime = parseFloat(starttime.toFixed(2));
+    }
+    alert(this.source_video);
+    if (this.source_video != " "){
+      this.showVideoOverlay = true;
+    }
+  }
+
+  closeVideoOverlay(): void {
+    this.showVideoOverlay = false;
+    // // Optionally, you can also pause the video when closing the overlay
+    // const videoElement: HTMLVideoElement = document.getElementById('videoPlayer') as HTMLVideoElement;
+    // videoElement.pause();
+    this.source_video = " ";
   }
 
   toggleImageSelection(src: string): void {
@@ -37,8 +80,8 @@ export class SearchedOutputComponent {
   isImageSelected(src: string): boolean {
     return this.selectedImages.includes(src);
   }
-  
-  search(): void{
+
+  search(): void {
 
   }
 }
