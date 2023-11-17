d228706514a5a62b5df38dee2907c8a3b865f53b
Abbas Haider <a.haider@qub.ac.uk>
2023-11-17 14:24:22 +0000

    Input textbox autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { DataSharingServiceService } from './data-sharing-service.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorHandler } from '@angular/core';
 import { GlobalErrorHandler } from './error-handler.service';
 import { MatDialogModule } from '@angular/material/dialog';
 import { VideoDialogComponent } from './video-dialog/video-dialog.component';
 import { SoundEventDialogComponent } from './sound-event-dialog/sound-event-dialog.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -36,6 +38,9 @@ import { SoundEventDialogComponent } from './sound-event-dialog/sound-event-dial
     FormsModule,
     MatDialogModule,
     MatSelectModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [ImageBasedSearchService, DataSharingServiceService, { provide: ErrorHandler, useClass: GlobalErrorHandler }],
   bootstrap: [AppComponent]
